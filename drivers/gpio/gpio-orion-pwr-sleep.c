a3da1f01def285825f859832c55c7706c2071b5a
Vladimir Hrechko <grechko_vo@navis.ru>
2021-03-09 16:13:40 +0300

    Wakeup form GNSS UART. Just it works!

--- drivers/gpio/gpio-orion-pwr-sleep.c
+++ drivers/gpio/gpio-orion-pwr-sleep.c
@@ -8,9 +8,9 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 
-#define PAD_CONF_MCBSP3_FSX_ADDRESS (0x48002172)
-#define PAD_CONF_SET_UART2_RX       (0x119)
-#define PAD_CONF_SET_GPIO_147_INPUT (0x11C)
+#define PAD_CONF_MCBSP3_FSX_ADDRESS       (0x48002172)
+#define PAD_CONF_SET_UART2_RX             (0x119)
+#define PAD_CONF_SET_GPIO_143_INPUT       (0x411C)
 
 #define PRCM_CM_ICLKEN_PER_ADDRESS        (0x48005010)
 #define GPIO_5_GPIO_OE_ADDRESS            (0x49056034)
@@ -19,8 +19,8 @@
 #define GPIO_5_GPIO_WAKEUPENABLE_ADDRESS  (0x49056020)
 #define GPIO_5_GPIO_SYSCONFIG_ADDRESS     (0x49056010)
 
-#define GPIO_5_GPIO_IRQENABLE1    (0x4905601C)
-#define GPIO_5_GPIO_IRQENABLE2    (0x4905602C)
+#define GPIO_5_GPIO_IRQENABLE1            (0x4905601C)
+#define GPIO_5_GPIO_IRQENABLE2            (0x4905602C)
 
 struct gu_pinmux_data {
   void __iomem *pad_conf;
@@ -31,7 +31,44 @@ struct gu_pinmux_data {
 static irqreturn_t gpio_hndl(int a, void *b)
 {
   printk(KERN_DEBUG "IRQ GPIO 5\n");
+
   return IRQ_HANDLED;
+}
+
+
+#define SET_REG_BIT_OR  0
+#define SET_REG_BIT_AND 1
+#define SET_REG_SET_ALL 2
+static int set_register(struct device	*dev, u32 adr, u32 reg, char *descr, int type)
+{
+  void __iomem *vm_register = ioremap(adr, 4);
+  u32 cur_reg = 0;
+
+  if(vm_register == NULL){
+    if( descr != NULL )
+      dev_err(dev, "cann't remap %s 0x%08X\n", descr, adr);
+    else
+      dev_err(dev, "cann't remap 0x%08X\n", adr);
+    return (-ENXIO);
+  } else {
+    cur_reg = readl(vm_register);
+    switch(type)
+    {
+      case SET_REG_BIT_OR:
+        cur_reg |= reg;
+        break;
+      case SET_REG_BIT_AND:
+        cur_reg &= ~reg;
+        break;
+      default: cur_reg = reg;
+    }
+    writel(cur_reg, vm_register);
+    if( descr != NULL )
+      dev_dbg(dev, "%s:   %08X\n", descr, readl(vm_register));
+    iounmap(vm_register);
+  }
+
+  return 0;
 }
 
 static int gpio_uart_pinmux_probe(struct platform_device *pdev)
@@ -44,6 +81,26 @@ static int gpio_uart_pinmux_probe(struct platform_device *pdev)
 
   dev_dbg(device, "probe starts\n");
 
+  /*if( (err = set_register(device, PRCM_CM_ICLKEN_PER_ADDRESS,  // Unlock GPIO 5
+                          (1 << 16), "CM_ICLKEN", SET_REG_BIT_OR) ) != 0)
+  return err;
+
+  if( (err = set_register(device, GPIO_5_GPIO_OE_ADDRESS,
+                          (1 << 15), "GPIO_OE", SET_REG_BIT_OR) ) != 0)
+  return err;
+
+  if( (err = set_register(device, GPIO_5_GPIO_FALLINGDETECT_ADDRESS,
+                          (1 << 15), "GPIO_FALLINGDETECT", SET_REG_BIT_OR) ) != 0)
+  return err;
+
+  if( (err = set_register(device, GPIO_5_GPIO_WAKEUPENABLE_ADDRESS,
+                          (1 << 15), "GPIO_WAKEUPENABLE", SET_REG_BIT_OR) ) != 0)
+  return err;
+
+  if( (err = set_register(device, GPIO_5_GPIO_SYSCONFIG_ADDRESS,
+                          (1 << 2), "GPIO_SYSCONFIG", SET_REG_BIT_OR) ) != 0)
+  return err;*/
+
   data = kzalloc(sizeof(*data), GFP_KERNEL);
   if(data == NULL){
     dev_err(device, "no memory\n");
@@ -54,149 +111,36 @@ static int gpio_uart_pinmux_probe(struct platform_device *pdev)
   if(data->pad_conf == NULL){
     dev_err(device, "cann't remap PAD_CONF_MCBSP3_FSX (0x%08X)\n",
                      PAD_CONF_MCBSP3_FSX_ADDRESS);
-    err = (-ENXIO);
-    goto err_fail;
+    kfree(data);
+    return (-ENXIO);
   }
 
-  vm_register = ioremap(PRCM_CM_ICLKEN_PER_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap PRCM_CM_ICLKEN_PER (0x%08X)\n",
-                     PRCM_CM_ICLKEN_PER_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 16);      // set EN_GPIO5
-    writel(reg, vm_register);
-    dev_dbg(device, "CM_ICLKEN:   %08X\n", readl(vm_register));
-    writew(PAD_CONF_SET_GPIO_147_INPUT, data->pad_conf);
-    iounmap(vm_register);
+  irq = platform_get_irq(pdev, 0);
+  if( irq <= 0 ) {
+    dev_err(device, "No free IRQs\n");
+    kfree(data);
+    return (-ENXIO);
   }
 
-  vm_register = ioremap(GPIO_5_GPIO_OE_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_OE (0x%08X)\n",
-                     GPIO_5_GPIO_OE_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_OE:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
+  printk(KERN_DEBUG "PINMUX IRQ id: %d\n", irq);
 
-  vm_register = ioremap(GPIO_5_GPIO_FALLINGDETECT_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_FALLINGDETECT (0x%08X)\n",
-                     GPIO_5_GPIO_FALLINGDETECT_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_FALLINGDETECT:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
+  err = devm_request_threaded_irq(device, irq,
+					  NULL, gpio_hndl,
+					  IRQF_SHARED | IRQF_ONESHOT,
+					  "gu-pinmux-hello", data);
 
-  vm_register = ioremap(GPIO_5_GPIO_RISINGDETECT_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_RISINGDETECT (0x%08X)\n",
-                     GPIO_5_GPIO_RISINGDETECT_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_RISINGDETECT:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
-
-  vm_register = ioremap(GPIO_5_GPIO_WAKEUPENABLE_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_WAKEUPENABLE (0x%08X)\n",
-                     GPIO_5_GPIO_WAKEUPENABLE_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_WAKEUPENABLE:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
-
-  vm_register = ioremap(GPIO_5_GPIO_SYSCONFIG_ADDRESS, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_SYSCONFIG (0x%08X)\n",
-                     GPIO_5_GPIO_SYSCONFIG_ADDRESS);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 2);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_SYSCONFIG:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
-
-  vm_register = ioremap(GPIO_5_GPIO_IRQENABLE1, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_IRQENABLE1 (0x%08X)\n",
-                     GPIO_5_GPIO_IRQENABLE1);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_5_GPIO_IRQENABLE1:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
-
-  vm_register = ioremap(GPIO_5_GPIO_IRQENABLE2, 4);
-  if(vm_register == NULL) {
-    dev_err(device, "cann't remap GPIO_5_GPIO_IRQENABLE1 (0x%08X)\n",
-                     GPIO_5_GPIO_IRQENABLE2);
-    iounmap(data->pad_conf);
-    err = (-ENXIO);
-    goto err_fail;
-  } else {
-    reg = readl(vm_register);
-    reg |= (1 << 19);          // set OUTPUTEN[19] = 0 - config GPIO 147 like input
-    writel(reg, vm_register);
-    dev_dbg(device, "GPIO_5_GPIO_IRQENABLE2:   %08X\n", readl(vm_register));
-    iounmap(vm_register);
-  }
+            if (err) {
+          		dev_err(device, "Unable to claim irq %d; error %d\n",
+          			irq, err);
+                kfree(data);
+          		return err;
+          	}
 
   platform_set_drvdata(pdev, data);
 
-  free_irq(33, NULL);
-
-  err = request_irq( 33,
-			               gpio_hndl,
-			               IRQF_SHARED,
-                     "gpio_5_handler",
-			               pdev );
-  dev_dbg(device, "SET GPIO IRQ:   %d\n", err);
 
   dev_dbg(device, "probe successfully ends\n");
   return 0;
-
-err_fail:
-  dev_dbg(device, "FAIL!!!\n");
-  kfree(data);
-  return err;
 }
 
 static int gpio_uart_pinmux_remove(struct platform_device *pdev)
@@ -215,7 +159,7 @@ static int gpio_uart_pinmux_suspend(struct platform_device *pdev, pm_message_t s
   struct gu_pinmux_data *data = platform_get_drvdata(pdev);
 
   dev_dbg(device, "suspend %d\n", state.event);
-  writew(PAD_CONF_SET_GPIO_147_INPUT, data->pad_conf);
+  writew(PAD_CONF_SET_GPIO_143_INPUT, data->pad_conf);
   /*if( !(data->count % 2) ) {
     dev_dbg(device, "reconfig uart_rx->gpio_147\n");
     writew(PAD_CONF_SET_GPIO_147_INPUT, data->pad_conf);
